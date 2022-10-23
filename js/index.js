d475d82df0bae6990dbf01e0d53da168800c22f2
时迁酱 <2115295703@qq.com>
2022-10-23 11:21:13 +0800

    11123

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ time();
 /* 计算运行天数的封装函数 */
 function DateDiff(sDate1, sDate2) {
   //sDate1和sDate2是2002-12-18格式
+  
   var aDate, oDate1, oDate2, iDays;
   aDate = sDate1.split("-");
   oDate1 = new Date(aDate[1] + "-" + aDate[2] + "-" + aDate[0]); //转换为12-18-2002格式
